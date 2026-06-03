bc6f2d4e8f52354ec9ba9052a56a584a8e20bca1
Aditya <adityamaske2022@gmail.com>
2026-06-04 01:48:18 +0530

    Complete Google JWT login flow

--- server/src/routes/googleAuthRoutes.js
+++ server/src/routes/googleAuthRoutes.js
@@ -4,20 +4,30 @@ const passport = require("passport");
 const router = express.Router();
 
 router.get(
-  "/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"],
-  })
+    "/google",
+    passport.authenticate("google", {
+        scope: ["profile", "email"],
+    })
 );
 
 router.get(
-  "/google/callback",
-  passport.authenticate("google", {
-    failureRedirect: "/login",
-  }),
-  (req, res) => {
-    res.send("Google Login Success");
-  }
-);
+    "/google/callback",
+    passport.authenticate("google", {
+        failureRedirect: "/login",
+    }),
+    (req, res) => {
+
+        const jwt = require("jsonwebtoken");
+
+        const token = jwt.sign(
+            { id: req.user._id },
+            "secretkey",
+            { expiresIn: "7d" }
+        );
+        console.log("TOKEN GENERATED:", token);
+        res.redirect(
+            `http://localhost:5173/google-success?token=${token}`
+        );
+});
 
 module.exports = router;
